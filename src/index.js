bb2d190bdd7d31ac5ecc2d02f451fe4a624b2e7f
Vladislav <svialence@yandex.ru>
2022-10-11 05:01:13 +0300

    import project layout

--- src/index.js
+++ src/index.js
@@ -1,7 +1,46 @@
 // import 'javascripts/main'
-import "stylesheets/base.scss";
-const template = require("./views/template.hbs");
+import 'stylesheets/base.scss'
 
-document.addEventListener("DOMContentLoaded", function () {
-  document.body.innerHTML = template({header: "page A"});
-});
+const home = require('./views/pages/home/home.hbs')
+
+document.addEventListener('DOMContentLoaded', function () {
+  const offers = [
+    {
+      title: 'Легковые автомобили',
+      inKirov: '5000',
+      image: '/images/offers/1.png',
+    },
+    {
+      title: 'Коммерческий транспорт',
+      inKirov: '5000',
+      image: '/images/offers/2.png',
+    },
+    {
+      title: 'Спецтехника',
+      inKirov: '5000',
+      image: '/images/offers/3.png',
+    },
+    {
+      title: 'Жилая недвижимость',
+      inKirov: '5000',
+      image: '/images/offers/4.png',
+    },
+    {
+      title: 'Коммерческая недвижимость',
+      inKirov: '5000',
+      image: '/images/offers/5.png',
+    },
+    {
+      title: 'Земельные участки',
+      inKirov: '5000',
+      image: '/images/offers/6.png',
+    },
+    {
+      title: 'Станки и оборудование',
+      inKirov: '5000',
+      image: '/images/offers/7.png',
+    },
+  ]
+
+  document.body.innerHTML = home({ offers })
+})
