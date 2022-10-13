881b46d2f52881c8c294acc43adc853921a2968a
Vladislav <svialence@yandex.ru>
2022-10-13 15:42:46 +0300

    remove unused

--- src/index.js
+++ src/index.js
@@ -6,50 +6,13 @@ const SearchResults = require('./views/pages/search-results/SearchResults.hbs')
 const SearchResultsLines = require('./views/pages/search-results-lines/SearchResultsLines.hbs')
 const Offer = require('./views/pages/offer/Offer.hbs')
 const Rate = require('./views/pages/rate/Rate.hbs')
-const offers = [
-  {
-    title: 'Легковые автомобили',
-    inKirov: '5000',
-    image: '/images/home/offers/1.png',
-  },
-  {
-    title: 'Коммерческий транспорт',
-    inKirov: '5000',
-    image: '/images/home/offers/2.png',
-  },
-  {
-    title: 'Спецтехника',
-    inKirov: '5000',
-    image: '/images/home/offers/3.png',
-  },
-  {
-    title: 'Жилая недвижимость',
-    inKirov: '5000',
-    image: '/images/home/offers/4.png',
-  },
-  {
-    title: 'Коммерческая недвижимость',
-    inKirov: '5000',
-    image: '/images/home/offers/5.png',
-  },
-  {
-    title: 'Земельные участки',
-    inKirov: '5000',
-    image: '/images/home/offers/6.png',
-  },
-  {
-    title: 'Станки и оборудование',
-    inKirov: '5000',
-    image: '/images/home/offers/7.png',
-  },
-]
 
 document.addEventListener('DOMContentLoaded', function () {
   switch (document.location.pathname) {
     case "/":
       break;
     case "/home":
-      document.body.innerHTML = home({ offers })
+      document.body.innerHTML = home()
       break;
 
     case "/search-results":
