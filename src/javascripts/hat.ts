4e519bdaefeee07c6ea1d11af67075c438ba7850
Vladislav <svialence@yandex.ru>
2022-10-18 08:35:00 +0300

    add datepicker in search from

--- src/javascripts/hat.ts
+++ src/javascripts/hat.ts
@@ -3,11 +3,13 @@ import $ from "jquery";
 $(() => {
   const $bg = $(".searchMenuBackground");
   const $searchForm = $(".hatSearchForm");
+  const $hat = $(".hat");
   const $body = $(document.body);
 
   function show() {
     if ($bg.hasClass("searchMenuBackground_active")) {
       hide();
+      return;
     }
 
     $searchForm.show();
@@ -26,9 +28,10 @@ $(() => {
     show();
   });
 
-  $searchForm.on("click", (e) => {
-    e.stopPropagation();
-  })
+  // $searchForm.on("click", (e) => {
+  //   e.stopPropagation();
+  // })
 
-  $body.on("click", hide);
+  $bg.on("click", hide);
+  $hat.on("click", hide);
 });
