c6f3333bde4969eb3f9d914c26b9a3f111629347
Vladislav <svialence@yandex.ru>
2022-10-28 16:45:12 +0300

    remove button only for multiple

--- src/javascripts/ui/Select.ts
+++ src/javascripts/ui/Select.ts
@@ -2,7 +2,15 @@ import $ from "jquery";
 import "@selectize/selectize";
 
 $(() => {
-  $('.baseSelect').selectize({
-    plugins: ["remove_button"],
-  });
+  $('.baseSelect').each((i, e) => {
+    const $select = $(e);
+
+    if ($select.prop("multiple")) {
+      $select.selectize({
+        plugins: ["remove_button"],
+      });
+    } else {
+      $select.selectize();
+    }
+  })
 });
